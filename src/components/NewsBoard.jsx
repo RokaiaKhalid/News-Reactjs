41ca809561a3d93f7d9ad7e3e023e9a6b22ccfb0
rokaia23 <rokaiak308@gmail.com>
2024-08-02 12:22:54 +0300

    processing

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -6,7 +6,8 @@ const NewsBoard = ({category}) => {
 
     useEffect(() => {
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response => response.json()).then(data => setArticles(data.articles)).catch(rej => console.log(rej));
+      fetch(url).then(response => response.json()).then(data => setArticles(data.articles))
+      .catch(error => console.error('Error fetching data:', error));
 
     },[category]) 
 
@@ -14,7 +15,7 @@ const NewsBoard = ({category}) => {
     <div className="container">
       <h2 className="text-center">Latest <span className="badge bg-danger ">News</span></h2>
       <div className="news">
-        {articles && articles.map((news, index) => (
+        {articles.map((news, index) => (
            <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
         ))}
       </div>
